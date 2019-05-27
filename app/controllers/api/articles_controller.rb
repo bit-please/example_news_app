d1165ba3013d9c901b316db290e24dd734d84a5c
danizaghian <dani.zaghian@gmail.com>
2019-05-27 11:13:51 -0700

    makes controller HTTP request easier to read

--- app/controllers/api/articles_controller.rb
+++ app/controllers/api/articles_controller.rb
@@ -2,7 +2,12 @@ class Api::ArticlesController < ApplicationController
 
 	def index
 		search_term = params[:search]
-		@articles = HTTP.headers({"X-Api-Key" => ENV["api_key"]}).get("https://newsapi.org/v2/everything?q=#{search_term}").parse["articles"]
+		@articles = HTTP
+			.headers({
+				"X-Api-Key" => ENV["api_key"]
+			})
+			.get("https://newsapi.org/v2/everything?q=#{search_term}")
+			.parse["articles"]
 		render 'index.json.jbuilder'
 	end
 
